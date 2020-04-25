51f55efba45fad25df0a60d105cba552429ca3ba
Farouk <Farouk>
2020-04-25 22:23:13 +0300

    done

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 
-
+import config_file;
+import import_ipynb;
+import main;
 
 if __name__ == '__main__':
-    print('hello world');
+    configs = config_file.readConfigs();
 
-    
+    mode = configs['mode']
+    print('mode:', mode);
+
+    if(mode == 'online'):
+        pass;
+    elif(mode == 'offline'):
+        pass;
+    elif(mode == 'webcam'):
+        pass;
+    else:
+        video = LocalFaceDetector(WebCam());
+        video.download(display=True, limit=1000);
+
